86a5599ddf966f51ca814f14a1ed1ac04c1ea7a2
Kent C. Dodds <kent+github@doddsfamily.us>
2015-02-03 11:28:11 +0100

    Step 2 improved

---
+++ app/root/components/DataGrid.js
@@ -0,0 +1,52 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var _ = require('lodash');
+
+require('./DataGrid.css');
+
+var DataGrid = React.createClass({
+  render() {
+    var data = this.props.data;
+    var max = _.chain(data).flatten().max().value();
+    var high = max * 0.75;
+    var low = max * 0.25;
+    var columnCount = data[0].length;
+    var rowCount = data.length;
+    var columnHeaders = _.range(columnCount);
+    var columns = columnHeaders.map(header => <th key={header}>Column {header + 1}</th>);
+
+    var rows = data.map((row, index) => {
+      var cells = row.map((value, index) => {
+        var className = value > high ? 'high' : value < low ? 'low' : null;
+        return <td key={index} className={className}>{value}</td>;
+      });
+      return (
+        <tr key={index}>
+          <td><strong>Row {index + 1}</strong></td>
+          {cells}
+        </tr>
+      );
+    });
+
+    return (
+      <div className="DataGrid">
+        Hi! I'm a data grid! I have {columnCount} columns and {rowCount} rows!
+
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              {columns}
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
+
+module.exports = DataGrid;
